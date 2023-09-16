48b2433ed5f32b894a3e566dea31eea3bb2136ed
UmarBhutta <malik.umar@msn.com>
2023-09-16 22:55:03 +0200

    remove unused models

--- feature/customers_list/presentation/src/main/java/com/patronus/customers_list/presentation/ui/data/UserDetailsUiModel.kt
+++
@@ -1,9 +0,0 @@
-package com.patronus.customers_list.presentation.ui.data
-
-data class UserDetailsUiModel(
-    val name : String,
-    val email : String,
-    val website: String,
-    val companyDetails: String,
-    val address: String
-)
